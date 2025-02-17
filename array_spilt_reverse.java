ac8d0fcc48c907cf72ed2fdae21f1ab8e778fcc0
Sri-Prasanna9949 <prasannasakthivel2005@gmail.com>
2025-02-17 14:20:23 +0530

    Add files via upload

---
+++ array_spilt_reverse.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    int arr[]={1,2,3,4,5,6};
+	    int l=arr.length;
+	    int n=3;
+	    for(int i=n-1;i>=0;i--)
+	    {
+	        System.out.print(arr[i]);
+	    }
+	    for(int i=l-1;i>=n;i--)
+	    {
+	        System.out.print(arr[i]);
+	    }
+	    
+	}
+}
